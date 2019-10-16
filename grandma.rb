b085496aa23145342758b315ab05ea0d262b865b
daniel arzayus <randomblue1@yahoo.com>
2019-10-16 03:32:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma (statement)
-if "I LOVE YOU GRANDMA!" 
-  puts "I LOVE YOU TOO PUMPKIN!"
+if "I LOVE YOU GRANDMA!" == "I LOVE YOU TOO PUMPKIN!"
 
 elsif statement == statement.downcase 
 puts "HUH?! SPEAK UP, SONNY!"
