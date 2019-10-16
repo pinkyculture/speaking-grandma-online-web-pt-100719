f027bb4f791bf8d34661f4d3e78b1db393ab1b6c
daniel arzayus <randomblue1@yahoo.com>
2019-10-16 03:49:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,13 +4,11 @@ def speak_to_grandma (statement)
 if statement == "I LOVE YOU GRANDMA!" 
   return "I LOVE YOU TOO PUMPKIN!"
 
-elsif statement == statement.downcase 
-return "HUH?! SPEAK UP, SONNY!"
-
-
-else statement == statement.upcase 
+elsif statement == statement.upcase 
   return "NO, NOT SINCE 1938!"
-  
+
+
+else 
   end
 end
 
